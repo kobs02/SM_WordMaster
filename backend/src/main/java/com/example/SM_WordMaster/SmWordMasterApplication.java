738df933665f1b8f408f89d05a9a4549d29f5034
kobs02 <7e001ab@gmail.com>
2025-03-25 08:46:32 +0900

    test

---
+++ backend/src/main/java/com/example/SM_WordMaster/SmWordMasterApplication.java
@@ -0,0 +1,13 @@
+package com.example.SM_WordMaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmWordMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmWordMasterApplication.class, args);
+	}
+
+}
