ba35ea071c4b2b3f1da59c9a1ae4d45a95cc3f04
202310957 <snu11025@naver.com>
2025-05-08 23:30:54 +0900

    feat: 예문 생성, 조회, 삭제 기능 구현 (OpenAI API 연동)

--- backend/src/main/java/com/example/SMU_WordMaster/SmuWordMasterApplication.java
+++ backend/src/main/java/com/example/SMU_WordMaster/SmuWordMasterApplication.java
@@ -3,8 +3,9 @@ package com.example.SMU_WordMaster;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class SmuWordMasterApplication {
 	public static void main(String[] args) {
 		Dotenv dotenv = Dotenv.configure()
@@ -15,6 +16,7 @@ public class SmuWordMasterApplication {
 		System.setProperty("DB_URL", dotenv.get("DB_URL"));
 		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
 		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+		System.setProperty("OPENAI_API_KEY", dotenv.get("OPENAI_API_KEY"));
 
 		SpringApplication.run(SmuWordMasterApplication.class, args);
 	}
