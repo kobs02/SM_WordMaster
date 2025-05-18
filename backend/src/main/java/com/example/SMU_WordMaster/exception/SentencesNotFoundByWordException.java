b6f6b62b4e5130a6ac11d94acfedc3ebfe6aed36
이예니 <126794376+LeeYeni@users.noreply.github.com>
2025-05-19 03:43:25 +0900

    Delete backend/src/main/java/com/example/SMU_WordMaster/exception directory

--- backend/src/main/java/com/example/SMU_WordMaster/exception/SentencesNotFoundByWordException.java
+++
@@ -1,7 +0,0 @@
-package com.example.SMU_WordMaster.exception;
-
-public class SentencesNotFoundByWordException extends RuntimeException {
-    public SentencesNotFoundByWordException(String word) {
-        super("해당 단어에 대한 예문이 존재하지 않습니다: " + word);
-    }
-}
