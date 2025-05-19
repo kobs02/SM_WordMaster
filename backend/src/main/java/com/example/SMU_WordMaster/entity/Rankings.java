4a2b170762f744e027b3a0af05b393d164effb16
202310957 <snu11025@naver.com>
2025-05-20 02:14:07 +0900

    fix: Rankings, Words 엔티티 수정

--- backend/src/main/java/com/example/SMU_WordMaster/entity/Rankings.java
+++ backend/src/main/java/com/example/SMU_WordMaster/entity/Rankings.java
@@ -2,6 +2,7 @@ package com.example.SMU_WordMaster.entity;
 
 import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
